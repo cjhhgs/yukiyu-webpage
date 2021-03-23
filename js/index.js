3fb0e9e6950b7f5c7aee2384a01f49c109c957bd
zlyang <565213263@qq.com>
2021-03-23 11:27:43 +0800

    mousescroll

--- js/index.js
+++ js/index.js
@@ -3,13 +3,24 @@ var search = document.querySelector('.search');
 var chevronLeft = document.querySelector('.left-band').querySelector('.chevron-left');
 var mainPage = document.querySelector('#main-page');
 
-chevronLeft.addEventListener('click', function () {
-    animate(leftBand, -leftBand.offsetWidth);
-    animate(search, search.parentNode.offsetWidth, showMain);
-})
+chevronLeft.addEventListener('click', showMain)
 
-function showMain(){
-    mainPage.style.display = 'block';
+document.addEventListener('DOMMouseScroll', showMain, false);
+
+function showMain() {
+    animate(leftBand, -leftBand.offsetWidth);
+    animate(search, search.parentNode.offsetWidth, fadeIn(mainPage, 20));
+}
+
+function fadeIn(element, speed) {
+    element.style.display = 'block';
+    var speed = speed || 30;
+    var num = 0;
+    var st = setInterval(function () {
+        num++;
+        element.style.opacity = num / 100;
+        if (num == 100) { clearInterval(st); }
+    }, speed);
 }
 
 function animate(obj, target, callback) {
@@ -17,10 +28,10 @@ function animate(obj, target, callback) {
     obj.timer = setInterval(function () {
         // var step = Math.ceil((target - obj.offsetLeft) / 10);
         var step = (target - obj.offsetLeft) / 10;
-        step = step > 0 ? Math.ceil(step):Math.floor(step);
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
         if (obj.offsetLeft == target) {
             clearInterval(obj.timer);
-            if(callback){
+            if (callback) {
                 callback();
             }
         }
