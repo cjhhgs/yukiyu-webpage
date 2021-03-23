37e13c447a7d0817eae67e2b07bee0ecc6d0f088
zlyang <565213263@qq.com>
2021-03-23 10:46:03 +0800

    Vue test

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
 var leftBand = document.querySelector('.left-band');
 var search = document.querySelector('.search');
 var chevronLeft = document.querySelector('.left-band').querySelector('.chevron-left');
+var mainPage = document.querySelector('.main-page');
+
+var mainControler = new Vue({
+    el: "#main",
+    data: {
+        show: false
+    },
+    methods: {
+        handleClick: function(){
+            this.show = !this.show;
+        }
+    }
+})
+
 
 chevronLeft.addEventListener('click', function () {
     animate(leftBand, -leftBand.offsetWidth);
-    console.log(search.offsetLeft, search.offsetWidth);
-    animate(search, search.offsetRight+search.offsetLeft+search.offsetWidth);
+    // console.log(search.offsetLeft);
+    animate(search, search.parentNode.offsetWidth);
+    // mainControler.methods.handleClick();
 })
 
 function animate(obj, target, callback) {
     clearInterval(obj.timer);
     obj.timer = setInterval(function () {
-        // var step = Math.ceil((target - obj.offsetLeft) / 10);
         var step = (target - obj.offsetLeft) / 10;
         step = step > 0 ? Math.ceil(step):Math.floor(step);
         if (obj.offsetLeft == target) {
@@ -23,3 +37,4 @@ function animate(obj, target, callback) {
         obj.style.left = obj.offsetLeft + step + 'px';
     }, 15);
 }
+
