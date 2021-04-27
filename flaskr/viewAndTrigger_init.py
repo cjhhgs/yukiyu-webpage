9b25a68c36a56918b8fdd1e30445b25293819957
zlyang <565213263@qq.com>
2021-04-27 11:24:34 +0800

    start view create program

---
+++ flaskr/viewAndTrigger_init.py
@@ -0,0 +1,3 @@
+# select distinct name, company_name, conduct_name, img
+# from (((bangumi_list natural join bangumi_company) natural join bangumi_conduct)
+# 		natural join company) natural join conduct;
